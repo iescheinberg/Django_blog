9a2b4bbb06fab95915c17013a0c2c4f21949a90e
iescheinberg <agr.scheinberg@gmail.com>
2024-10-22 15:48:45 -0300

    first commit

---
+++ blog/apps/usuario/views.py
@@ -0,0 +1,35 @@
+from .forms import RegistroUsuarioForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import CreateView
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
+
+# Create your views here.
+
+class RegistrarUsuario(CreateView):
+    template_name = 'registration/registrar.html'
+    form_class = RegistroUsuarioForm
+    
+    def form_valid(self, form):
+        messages.success(self.request, 'Registro exitoso. Por favor, inicia sesión🙌')
+        form.save()
+        
+        return redirect('apps.usuario:registrar')
+
+
+    
+class LoginUsuario(LoginView):
+    template_name = 'registration/login.html'        
+    def get_success_url(self):
+        messages.success(self.request, 'Login existoso!🚀')    
+        return reverse('apps.usuario:login')
+
+
+
+class LogoutUsuario(LogoutView):
+    template_name = 'registration/logout.html' 
+    
+    def get_success_url(self):
+        messages.success(self.request, 'Hasta la próxima!👋')
+        return reverse('apps.usuario:logout')
